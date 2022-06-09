55afc63de9860074c41168342b0557a0e8eac637
Onur Dursun <58361733+onur55-tr@users.noreply.github.com>
2022-06-09 14:50:56 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,17 +18,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2l;
 	char	*str;
 
-	if (!s2)
-		return ((char *)s1);
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + sizeof(char));
 	if (!str)
 		return (str);
 	s1l = 0;
 	s2l = 0;
-	while (s1[s1l])
+	while (s1 && s1[s1l])
 		str[s2l++] = s1[s1l++];
 	s1l = 0;
-	while (s2[s1l])
+	while (s2 && s2[s1l])
 		str[s2l++] = s2[s1l++];
 	str[s2l] = '\0';
 	return (str);
